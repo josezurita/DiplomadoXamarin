d4c532e12cd12769616a6fb9874f3987f08d7d09
josezurita <jose.zurita@epn.edu.ec>
2017-06-03 08:55:52 -0500

    Aprobado Lab04

---
+++ Lab04/AndroidApp/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidApp
+{
+    [Activity(Label = "AndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            var Validator = new PCLProject.AppValidator(new AndroidDialog(this));
+            Validator.EMail = "jose.zurita@epn.edu.ec";
+            Validator.Password = "***********";
+            Validator.Device = Android.Provider.Settings.Secure.GetString(
+                ContentResolver,
+                Android.Provider.Settings.Secure.AndroidId);
+            Validator.Validate();
+        }
+    }
+}
+
