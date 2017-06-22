7b130813a58fddf9f825428c4398bfa3f68f0d91
josezurita <jose.zurita@epn.edu.ec>
2017-06-22 16:15:06 -0500

    Añadidas carpetas de retos Xamarin Alliance y agregado botón de historial de llamadas en Lab06

---
+++ Lab06/PhoneApp/MainActivity.cs
@@ -0,0 +1,78 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace PhoneApp
+{
+    [Activity(Label = "Phone App", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView (Resource.Layout.Main);
+
+            var PhoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            var TranslateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            var CallButton = FindViewById<Button>(Resource.Id.CallButton);
+            string Device;
+
+            Device = Android.Provider.Settings.Secure.GetString(
+                ContentResolver,
+                Android.Provider.Settings.Secure.AndroidId);
+            Validate(Device);
+            
+            CallButton.Enabled = false;
+
+            var TranslatedNumber = string.Empty;
+
+            TranslateButton.Click += (object sender, System.EventArgs e) =>
+            {
+                var Translator = new PhoneTranslator();
+                TranslatedNumber = Translator.ToNumber(PhoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(TranslatedNumber))
+                {
+                    //No hay número a llamar
+                    CallButton.Text = "Llamar";
+                    CallButton.Enabled = false;
+                }
+                else
+                {
+                    //Hay un posible número telefónico a llamar
+                    CallButton.Text = $"Llamar al {TranslatedNumber}";
+                    CallButton.Enabled = true;
+                }
+            };
+
+            CallButton.Click += (object sender, System.EventArgs e) =>
+            {
+                //Intentar marcar el número telefónico
+                var CallDialog = new AlertDialog.Builder(this);
+                CallDialog.SetMessage($"Llamar al número {TranslatedNumber}?");
+                CallDialog.SetNeutralButton("Llamar", delegate
+                {
+                    //Crear un intento para marcar el número telefónico
+                    var CallIntent = new Android.Content.Intent(Android.Content.Intent.ActionCall);
+                    CallIntent.SetData(Android.Net.Uri.Parse($"tel:{TranslatedNumber}"));
+                    StartActivity(CallIntent);
+                });
+                CallDialog.SetNegativeButton("Cancelar", delegate { });
+                CallDialog.Show();
+            };
+        }
+
+        public async void Validate(string device)
+        {
+            string Result;
+            var ValidationText = FindViewById<TextView>(Resource.Id.ValidationText);
+            var ServiceClient = new SALLab06.ServiceClient();
+            var SvcResult = await ServiceClient.ValidateAsync(
+                "jose.zurita@epn.edu.ec",
+                "**************",
+                device);
+            Result = $"{SvcResult.Status}\n{SvcResult.Fullname}\n{SvcResult.Token}";
+            ValidationText.Text = Result;
+        }
+    }
+}
+
---
+++ XamarinAlliance/MyChallenge/XamarinChallenge3JZ_Runtime/XamarinChallenge3JZService/App_Start/Startup.MobileApp.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Web.Http;
+using Microsoft.Azure.Mobile.Server;
+using Microsoft.Azure.Mobile.Server.Authentication;
+using Microsoft.Azure.Mobile.Server.Config;
+using XamarinChallenge3JZService.DataObjects;
+using XamarinChallenge3JZService.Models;
+using Owin;
+
+namespace XamarinChallenge3JZService
+{
+    public partial class Startup
+    {
+        public static void ConfigureMobileApp(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            //For more information on Web API tracing, see http://go.microsoft.com/fwlink/?LinkId=620686 
+            config.EnableSystemDiagnosticsTracing();
+
+            new MobileAppConfiguration()
+                .UseDefaultConfiguration()
+                .ApplyTo(config);
+
+            // Use Entity Framework Code First to create database tables based on your DbContext
+            Database.SetInitializer(new XamarinChallenge3JZInitializer());
+
+            // To prevent Entity Framework from modifying your database schema, use a null database initializer
+            // Database.SetInitializer<XamarinChallenge3JZContext>(null);
+
+            MobileAppSettingsDictionary settings = config.GetMobileAppSettingsProvider().GetMobileAppSettings();
+
+            if (string.IsNullOrEmpty(settings.HostName))
+            {
+                // This middleware is intended to be used locally for debugging. By default, HostName will
+                // only have a value when running in an App Service application.
+                app.UseAppServiceAuthentication(new AppServiceAuthenticationOptions
+                {
+                    SigningKey = ConfigurationManager.AppSettings["SigningKey"],
+                    ValidAudiences = new[] { ConfigurationManager.AppSettings["ValidAudience"] },
+                    ValidIssuers = new[] { ConfigurationManager.AppSettings["ValidIssuer"] },
+                    TokenHandler = config.GetAppServiceTokenHandler()
+                });
+            }
+            app.UseWebApi(config);
+        }
+    }
+
+    public class XamarinChallenge3JZInitializer : CreateDatabaseIfNotExists<XamarinChallenge3JZContext>
+    {
+        protected override void Seed(XamarinChallenge3JZContext context)
+        {
+            List<TodoItem> todoItems = new List<TodoItem>
+            {
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "First item", Complete = false },
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "Second item", Complete = false },
+            };
+
+            foreach (TodoItem todoItem in todoItems)
+            {
+                context.Set<TodoItem>().Add(todoItem);
+            }
+
+            base.Seed(context);
+        }
+    }
+}
+
