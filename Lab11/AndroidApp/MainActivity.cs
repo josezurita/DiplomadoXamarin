a97a35f9f2bf6d35357bbb92a593aa4f0be91475
josezurita <jose.zurita@epn.edu.ec>
2017-06-29 11:58:18 -0500

    Finalizado Lab11

---
+++ Lab11/AndroidApp/MainActivity.cs
@@ -0,0 +1,111 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidApp
+{
+    [Activity(Label = "AndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Complex Data;
+        int Counter = 0;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView (Resource.Layout.Main);
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnCreate");
+
+            Data = (Complex)this.FragmentManager.FindFragmentByTag("Data");
+            if (Data == null)
+            {
+                Data = new Complex();
+                var FragmentTransaction = this.FragmentManager.BeginTransaction();
+                FragmentTransaction.Add(Data, "Data");
+                FragmentTransaction.Commit();
+            }
+
+            if (bundle != null)
+            {
+                Counter = bundle.GetInt("CounterValue", 0);
+                Android.Util.Log.Debug("Lab11Log", "Activity A - Recovered Instance State");
+            }
+
+            var ClickCounter = FindViewById<Button>(Resource.Id.ClicksCounter);
+            ClickCounter.Text = Resources.GetString(Resource.String.ClicksCounter_Text, Counter);
+            ClickCounter.Text += $"\n{Data.ToString()}";
+
+            ClickCounter.Click += (sender, e) =>
+            {
+                Counter++;
+                ClickCounter.Text = Resources.GetString(Resource.String.ClicksCounter_Text, Counter);
+
+                Data.Real++;
+                Data.Imaginary++;
+                ClickCounter.Text += $"\n{Data.ToString()}";
+            };
+
+            FindViewById<Button>(Resource.Id.StartActivity).Click += (sender, e) => {
+                var ActivityIntent =
+                new Android.Content.Intent(this, typeof(SecondActivity));
+                StartActivity(ActivityIntent);
+            };
+
+            Validate(Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId), "jose.zurita@epn.edu.ec", "**************");
+        }
+
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnSaveInstanceState");
+            outState.PutInt("CounterValue", Counter);
+            base.OnSaveInstanceState(outState);
+        }
+
+        protected override void OnStart()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnStart");
+            base.OnStart();
+        }
+
+        protected override void OnResume()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnResume");
+            base.OnResume();
+        }
+
+        protected override void OnPause()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnPause");
+            base.OnPause();
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnStop");
+        }
+
+        protected override void OnDestroy()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnDestroy");
+            base.OnDestroy();
+        }
+
+        protected override void OnRestart()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnRestart");
+            base.OnRestart();
+        }
+
+        public async void Validate(string device, string correo, string contrasena)
+        {
+            var ValidationText = FindViewById<TextView>(Resource.Id.ValidationText);
+            var ServiceClient = new SALLab11.ServiceClient();
+            var SvcResult = await ServiceClient.ValidateAsync(
+                correo,
+                contrasena,
+                device);
+            ValidationText.Text = $"{SvcResult.Fullname}\n{SvcResult.Status}\n{SvcResult.Token}";
+        }
+    }
+}
+
---
+++ Lab11/AndroidApp/SecondActivity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace AndroidApp
+{
+    [Activity(Label = "SecondActivity")]
+    public class SecondActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Android.Util.Log.Debug("Lab11Log", "Activity B - OnCreate");
+            // Create your application here
+        }
+
+        protected override void OnStart()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity B - OnStart");
+            base.OnStart();
+        }
+
+        protected override void OnResume()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity B - OnResume");
+            base.OnResume();
+        }
+
+        protected override void OnPause()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity B - OnPause");
+            base.OnPause();
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+            Android.Util.Log.Debug("Lab11Log", "Activity B - OnStop");
+        }
+
+        protected override void OnDestroy()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity B - OnDestroy");
+            base.OnDestroy();
+        }
+
+        protected override void OnRestart()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity B - OnRestart");
+            base.OnRestart();
+        }
+    }
+}
