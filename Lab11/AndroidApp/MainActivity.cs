7988e670ba7e5407419311927d4d9b13ed0b072e
josezurita <jose.zurita@epn.edu.ec>
2017-06-30 14:00:39 -0500

    Agregada persistencia de resultado de la validación

--- Lab11/AndroidApp/MainActivity.cs
+++ Lab11/AndroidApp/MainActivity.cs
@@ -8,6 +8,7 @@ namespace AndroidApp
     public class MainActivity : Activity
     {
         Complex Data;
+        ValidationResult Result;
         int Counter = 0;
         protected override void OnCreate(Bundle bundle)
         {
@@ -16,6 +17,19 @@ namespace AndroidApp
             Android.Util.Log.Debug("Lab11Log", "Activity A - OnCreate");
 
             Data = (Complex)this.FragmentManager.FindFragmentByTag("Data");
+            Result = (ValidationResult)this.FragmentManager.FindFragmentByTag("Result");
+            var ValidationText = FindViewById<TextView>(Resource.Id.ValidationText);
+
+            if (Result == null)
+            {
+                Result = new ValidationResult();
+                var FragmentTransactionResult = this.FragmentManager.BeginTransaction();
+                FragmentTransactionResult.Add(Result, "Result");
+                FragmentTransactionResult.Commit();
+                Validate(Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId), "jose.zurita@epn.edu.ec", "**************");
+            }
+            ValidationText.Text = Result.ToString();
+
             if (Data == null)
             {
                 Data = new Complex();
@@ -43,14 +57,9 @@ namespace AndroidApp
                 Data.Imaginary++;
                 ClickCounter.Text += $"\n{Data.ToString()}";
             };
-
             FindViewById<Button>(Resource.Id.StartActivity).Click += (sender, e) => {
-                var ActivityIntent =
-                new Android.Content.Intent(this, typeof(SecondActivity));
-                StartActivity(ActivityIntent);
+                
             };
-
-            Validate(Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId), "jose.zurita@epn.edu.ec", "**************");
         }
 
         protected override void OnSaveInstanceState(Bundle outState)
@@ -98,13 +107,13 @@ namespace AndroidApp
 
         public async void Validate(string device, string correo, string contrasena)
         {
-            var ValidationText = FindViewById<TextView>(Resource.Id.ValidationText);
             var ServiceClient = new SALLab11.ServiceClient();
             var SvcResult = await ServiceClient.ValidateAsync(
                 correo,
                 contrasena,
                 device);
-            ValidationText.Text = $"{SvcResult.Fullname}\n{SvcResult.Status}\n{SvcResult.Token}";
+
+            Result.SaveValidation(SvcResult);
         }
     }
 }
---
+++ Lab11/AndroidApp/ValidationResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace AndroidApp
+{
+    class ValidationResult:Fragment
+    {
+        string Fullname { get; set; }
+       SALLab11.Status Status { get; set; }
+        string Token { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Fullname}\n{Status}\n{Token}";
+        }
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            RetainInstance = true;
+        }
+
+        public void SaveValidation (SALLab11.ResultInfo result)
+        {
+            this.Fullname = result.Fullname;
+            this.Status = result.Status;
+            this.Token = result.Token;
+        }
+    }
+}
